6b9a226c987964bff7a999d4f411109c3044ac7a
shrey bhardwaj <21uec125@gmail.com>
2023-09-21 16:52:30 +0530

    final

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,3 +1,7 @@
 export default function AuthLayout({ children }) {
-  return <div className="flex items-center justify-center h-full">{children}</div>;
+  return (
+    <div className="h-full flex items-center justify-center">
+      {children}
+    </div>
+  );
 }
